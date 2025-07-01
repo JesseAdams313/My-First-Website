c2bcd2e6fbd03968610a77bbad5a8fbc02c17c75
JesseAdams313 <126463141+JesseAdams313@users.noreply.github.com>
2025-06-30 23:03:32 -0400

    Add project files.

---
+++ JS/script.js
@@ -0,0 +1,11 @@
+﻿//----------JavaScript File----------//
+
+
+// This is a simple JavaScript function that displays an alert message
+function sayHello()
+{
+    alert("Hello from your website!");
+}
+
+//----------JavaScript File END----------//
+
